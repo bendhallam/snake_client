fad20ac855ac02e08bd113f8219560b50249f576
Benjamin <bendhallam@gmail.com>
2024-07-10 22:58:52 -0600

    fixed slight mistake in play.js

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
-    host: localhost,
+    host: "localhost",
     port: 50541
   });
 
